d68f523e92ed7ed4ca6b3b2bfa28f74e5cec3e18
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2021-09-21 23:39:21 -0700

    aww yeah I made a circle

--- src/app/scene/scene.component.ts
+++ src/app/scene/scene.component.ts
@@ -46,6 +46,124 @@ export class SceneComponent implements AfterViewInit {
 
     // Clear the colour as well as the depth buffer.
     this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+    this.drawCircle(360, 100, this.gl.canvas.width/2, this.gl.canvas.height/2);
+  }
+
+  private drawCircle(resolution: number, radius: number, x: number, y: number): void {
+    //Vertex Shader
+    const vertexShader = this.gl.createShader(this.gl.VERTEX_SHADER);
+    if (!vertexShader) {
+      console.log("Failed to create vertex shader.");
+      return;
+    }
+
+    this.gl.shaderSource(vertexShader, [
+      'attribute vec2 position;',
+      'void main() {',
+      ' gl_Position = vec4(position, 0.0, 1.0);',
+      '}'
+    ].join('\n'));
+
+    this.gl.compileShader(vertexShader);
+
+    if (!this.gl.getShaderParameter(vertexShader, this.gl.COMPILE_STATUS)) {
+      console.log('An error occurred compiling the vertex shader: ' + this.gl.getShaderInfoLog(vertexShader));
+      this.gl.deleteShader(vertexShader);
+      return;
+    }
+
+    //Fragment Shader
+    const fragmentShader = this.gl.createShader(this.gl.FRAGMENT_SHADER);
+    if (!fragmentShader) {
+      console.log("Failed to create fragment shader.");
+      return;
+    }
+
+    this.gl.shaderSource(fragmentShader, [
+      'precision highp float;',
+      'uniform vec4 color;',
+      'void main() {',
+      ' gl_FragColor = color;',
+      '}'
+    ].join('\n'));
+
+    this.gl.compileShader(fragmentShader);
+
+    if (!this.gl.getShaderParameter(fragmentShader, this.gl.COMPILE_STATUS)) {
+      console.log('An error occurred compiling the fragment shader: ' + this.gl.getShaderInfoLog(fragmentShader));
+      this.gl.deleteShader(fragmentShader);
+      return;
+    }
+
+    //Initialize the Shader Program
+    const circleProgram = this.gl.createProgram();
+    if (!circleProgram) {
+      console.log("Failed to create shader program.");
+      return;
+    }
+
+    this.gl.attachShader(circleProgram, vertexShader);
+    this.gl.attachShader(circleProgram, fragmentShader);
+    this.gl.linkProgram(circleProgram);
+
+    if (!this.gl.getProgramParameter(circleProgram, this.gl.LINK_STATUS)) {
+      console.log('Unable to initialize the circle shader program: ' + this.gl.getProgramInfoLog(circleProgram));
+      return;
+    }
+
+    //Create ProgramInfo
+    const circleProgramInfo = {
+      program: circleProgram,
+      attribLocations: {
+        position: this.gl.getAttribLocation(circleProgram, 'position'),
+      },
+      uniformLocations: {
+        color: this.gl.getUniformLocation(circleProgram, 'color'),
+      },
+    }
+
+    //Initialize Buffer
+    const circleBuffer = this.gl.createBuffer();
+
+    this.gl.bindBuffer(this.gl.ARRAY_BUFFER, circleBuffer);
+
+    //Circle positions
+    const positions = [];
+
+    //Centre point
+    positions.push(this.convertCoordinate(x, this.gl.canvas.width)); //x
+    positions.push(this.convertCoordinate(y, this.gl.canvas.height)); //y
+
+    for(let i = 0; i<resolution+1; i++){
+      let angle = (i * 2 * Math.PI) / resolution;
+      let pointX = x + (radius * Math.cos(angle))
+      let pointY = y + (radius * -Math.sin(angle));
+
+      positions.push(this.convertCoordinate(pointX, this.gl.canvas.width));
+      positions.push(this.convertCoordinate(pointY, this.gl.canvas.height));
+    }
+
+    console.log(positions);
+
+    this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(positions), this.gl.STATIC_DRAW);
+
+    //Draw the circle
+    this.gl.useProgram(circleProgram);
+
+    //Color the circle white
+    circleProgramInfo.uniformLocations.color = this.gl.getUniformLocation(circleProgram, 'color');
+    this.gl.uniform4fv(circleProgramInfo.uniformLocations.color, [1, 1, 1, 1]);
+
+    circleProgramInfo.attribLocations.position = this.gl.getAttribLocation(circleProgram, 'position');
+    this.gl.enableVertexAttribArray(circleProgramInfo.attribLocations.position);
+    this.gl.vertexAttribPointer(circleProgramInfo.attribLocations.position, 2, this.gl.FLOAT, false, 0, 0);
+
+    this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, positions.length/2);
+  }
+
+  private convertCoordinate(coord: number, resolution: number): number {
+    return (coord - (resolution/2)) / (resolution/2);
   }
 
 }
