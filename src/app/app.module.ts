1b5e9a355157d1ef87cc506478e4582825b5708f
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2021-09-24 21:01:34 -0700

    looks nice

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,21 +9,31 @@ import {MatToolbarModule} from "@angular/material/toolbar";
 import {MatIconModule} from "@angular/material/icon";
 import {MatButtonModule} from "@angular/material/button";
 import {HttpClientModule} from "@angular/common/http";
+import { FooterComponent } from './footer/footer.component';
+import { ScoreComponent } from './score/score.component';
+import { InfoComponent } from './info/info.component';
+import {MatTabsModule} from "@angular/material/tabs";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
     AppComponent,
     SceneComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    ScoreComponent,
+    InfoComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
-  ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        AppRoutingModule,
+        HttpClientModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        MatTabsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
