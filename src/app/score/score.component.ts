e4b358cbc55131b4c2d3e6f1df550ce1e2b55973
Nicholas-C-Brown <nicholas.c.brown@alumni.ubc.ca>
2021-09-24 23:25:41 -0700

    linting check

--- src/app/score/score.component.ts
+++ src/app/score/score.component.ts
@@ -1,15 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-score',
   templateUrl: './score.component.html',
   styleUrls: ['./score.component.scss']
 })
-export class ScoreComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+export class ScoreComponent {
 
 }
